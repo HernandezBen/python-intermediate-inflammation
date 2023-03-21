133e6a85e670224b83b67330da6672d7bdfd7f24
Benjamin Hernandez <h.benjamin256@gmail.com>
2023-03-21 13:28:22 +0100

    Docstring improvements.

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array
+
+    :param data: 2d array containing the inflamation data
+    :returns: 1D array containing the mean of the data along the columns
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array
+
+    :param data: 2d array containing the inflamation data
+    :returns: 1D array containing the max of the data along the columns
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array
+
+    :param data: 2d array containing the inflamation data
+    :returns: 1D array containing the min of the data along the columns
+    """
     return np.min(data, axis=0)
 
